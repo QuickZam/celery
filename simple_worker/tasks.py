2935014471982b9c99e8c3a76db0d6da89e23280
QuickZam <111820344+QuickZam@users.noreply.github.com>
2022-10-29 18:53:12 +0530

    Update tasks.py

--- simple_worker/tasks.py
+++ simple_worker/tasks.py
@@ -13,8 +13,9 @@ logger = get_task_logger(__name__)
 app = Celery('tasks', broker='redis://redis:6379/0',
              backend='redis://redis:6379/0')
 
-api_key = "ec49909f-3d2f-4044-8882-535e3ce8a383"
-model_key = "7734639e-bcae-41f6-b7b9-47a9cbba26e1"
+   
+api_key =  ${{ secrets.API_KEY }}
+model_key = ${{ secrets.MODEL_KEY }}
 
 
 @app.task()
