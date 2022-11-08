b754df73e87de4ecb0232b7cc42785b978adb5aa
QuickZam <111820344+QuickZam@users.noreply.github.com>
2022-11-08 19:59:22 +0530

    added dev url to creds and sh file

--- simple_worker/creds.py
+++ simple_worker/creds.py
@@ -1,4 +1,7 @@
 api_key = "ec49909f-3d2f-4044-8882-535e3ce8a383"
 model_key = "7734639e-bcae-41f6-b7b9-47a9cbba26e1"
-url = "https://chitramai.com/api/1.1/obj/metadata/"
+url = "https://chitramai.com/version-test/api/1.1/obj"
 auth = 'Bearer e1a9185d16055bac44068c8ac1f0893a'
+
+
+url_live = "https://chitramai.com/api/1.1/obj/metadata/"
